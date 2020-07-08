0062f7c8be9825fc0921bf1a71a15672bb3dbda8
duatpwnd <duatpwnd1@naver.com>
2020-07-08 09:43:41 +0900

    postgre sql connection -3

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
 <script>
 export default {
   async asyncData({ app, query }) {
-    console.log('database 연결2222')
+    console.log('database 연결3333')
     const dbconnection = await app.$axios.$get(`/api/db_connect`)
   },
   data() {
